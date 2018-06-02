856a8bd98164e29ef5d5c510788329578f8a1b72
alvin joseph valdez <alvinjosephvaldez@Macintoshs-MacBook-Pro-3.local>
2018-06-02 17:21:37 +0800

    Added .gitignore

---
+++ Arsenal/GhostFormView.swift
@@ -0,0 +1,9 @@
+//
+//  GhostFormView.swift
+//  Arsenal
+//
+//  Created by alvin joseph valdez on 02/06/2018.
+//  Copyright © 2018 alvin joseph valdez. All rights reserved.
+//
+
+import Foundation
