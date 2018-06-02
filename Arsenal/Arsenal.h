25cb0d622fd0638728b67c0326d481f2a4341b62
alvin joseph valdez <alvinjosephvaldez@macintoshs-macbook-pro-3.local>
2018-06-02 16:48:26 +0800

    Initial Commit

---
+++ Arsenal/Arsenal.h
@@ -0,0 +1,19 @@
+//
+//  Arsenal.h
+//  Arsenal
+//
+//  Created by alvin joseph valdez on 02/06/2018.
+//  Copyright © 2018 alvin joseph valdez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Arsenal.
+FOUNDATION_EXPORT double ArsenalVersionNumber;
+
+//! Project version string for Arsenal.
+FOUNDATION_EXPORT const unsigned char ArsenalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Arsenal/PublicHeader.h>
+
+
